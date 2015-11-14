c866c2a41aa8b5e4d48a245e84ac30316c459153
Nigel Charman <nigel.charman.nz@gmail.com>
2015-11-14 21:30:41 +1300

    Remove unused code

--- src/main/java/org/concordion/markdown/MarkdownParser.java
+++ src/main/java/org/concordion/markdown/MarkdownParser.java
@@ -8,18 +8,13 @@ import org.pegdown.ToHtmlSerializer;
 import org.pegdown.ast.RootNode;
 
 public class MarkdownParser {
-    private PegDownProcessor processor = new PegDownProcessor(Extensions.TABLES);
-//    private ConcordionPluginParser mpp = Parboiled.createParser(ConcordionPluginParser.class);
-//    private PegDownPlugins plugins = new PegDownPlugins.Builder().withInlinePluginRules(mpp.concordionSetRule(), mpp.concordionEqualsRule()).build(); //Plugin(MyPluginParser.class, null).build();
-//    private PegDownPlugins plugins = new PegDownPlugins.Builder().withInlinePluginRules(mpp.concordionRules()).build(); //Plugin(MyPluginParser.class, null).build();
-//    private PegDownPlugins plugins = new PegDownPlugins.Builder().withBlockPluginRules(mpp.concordionStatement()).build(); //Plugin(MyPluginParser.class, null).build();
+    private PegDownProcessor processor = new PegDownProcessor();
     private Parser parser = Parboiled.createParser(Parser.class, Extensions.TABLES | Extensions.STRIKETHROUGH, 5000L, Parser.DefaultParseRunnerProvider); //, plugins);
 
     public String markdownToHtml(String markdown, String concordionNamespacePrefix) {
         RootNode root = parser.parse(processor.prepareSource(markdown.toCharArray()));
         ToHtmlSerializer serializer = new ConcordionHtmlSerializer(concordionNamespacePrefix);
         String html = serializer.toHtml(root);
-        System.out.println(html);
         return html;
     }
 }
